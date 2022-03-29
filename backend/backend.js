f89ef5e88cee813b1574333c9415262792a13ffd
srija1903 <64838154+srija1903@users.noreply.github.com>
2022-03-29 10:40:51 +0530

    Update backend.js

--- backend/backend.js
+++ backend/backend.js
@@ -2,104 +2,192 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { render } = require('@testing-library/react');
+require('dotenv').config();
+//const bcrypt = require('bcrypt');
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://srijasriram:yaariyan2@cluster0.wya1v.mongodb.net/test", {
-    dbName: "browser-buyer",
-    useNewUrlParser: true
-}).then(() => {
+mongoose.connect((process.env.MONGODB_URI || "mongodb+srv://srijasriram:yaariyan2@cluster0.wya1v.mongodb.net/test"),{
+    dbName:"browser-buyer",
+    useNewUrlParser:true
+}).then(() =>{
     console.log("Successfully connected");
-}).catch((err) => {
-    console.log("Error" + err);
+}).catch((err)=>{
+    console.log("Error" +err);
 });
 
-app.listen(8000, function() {
+app.listen((process.env.PORT || 8000),function() {
     console.log("App listening at port 8000");
 });
-const orderSchema = new mongoose.Schema({
-    date: {
-        type: "String",
-        required: true
+//Schema for creating and registering users
+const LoginSchema = new mongoose.Schema({
+    firstName:{
+        type:"String",
+        required:true
     },
-    skuid: {
-        type: "String",
-        required: true,
-        unique: true
+    lastName:{
+        type:"String",
+        required:true
     },
-    orderid: {
-        type: "String",
-        required: true,
-        unique: true
+    email:{
+        type:"String",
+        required:true
     },
-    productName: {
-        type: "String",
-        required: true
-    },
-    origin: {
-        type: "String",
-        required: true
-    },
-    price: {
-        type: "Number",
-        required: true
-    },
-    store1order: {
-        type: "Number",
-        required: true
-    },
-    store2order: {
-        type: "Number",
-        required: true
-    },
-    store3order: {
-        type: "Number",
-        required: true
-    },
-    buyerOrder: {
-        type: "Number"
+    password:{
+        type:"String",
+        required:true
     }
 });
-const orderModel = mongoose.model("orders", orderSchema);
-//To display the results on the main page
-app.get('/', (req, res) => {
+const LoginModel = mongoose.model("users",LoginSchema);
+//To register a user
+app.post('/register',(req,res) => {
 
-    orderModel.find({}, (error, result) => {
-        if (error) {
-            console.log("Display Error:" + error);
-        }
+    const firstName = req.body.firstName,lastName = req.body.lastName,email = req.body.email,password = req.body.password ;
+
+    const register = new LoginModel({firstName:firstName,
+        lastName:lastName,
+        email:email,
+        password:password
+    });
+
+    try {
+        register.save();
+        res.status(201).send("User Registered" + register);
+        console.log("Registered:"+ register);
+
+    } catch(error) {
+        console.log("Error"+error);
+    }
+});
+//to login 
+app.post("/login",(req,res) => {
+
+    const {email,password} = req.body;
+
+    try {
+        LoginModel.findOne({email:email},(err,user)=>{
+            if(user) {
+                if(password === user.password) {
+                    res.send({message:"login sucess"});
+                } else{
+                    res.send({message:"wrong credentials"});
+                }
+            } else {
+                res.send("not register");
+            }
+        });
+    } catch(error) {
+        console.log("Login Error:"+error);
+    }
+});
+//CRUD Operations on orders
+const orderSchema = new mongoose.Schema({
+    date:{
+        type:"String",
+        required:true
+    },
+    skuid:{
+        type:"String",
+        required:true,
+        unique:true
+    },
+    orderid:{
+        type:"String",
+        required:true,
+        unique:true
+    },
+    productName:{
+        type:"String",
+        required:true
+    },
+    origin:{
+        type:"String",
+        required:true
+    },
+    price:{
+        type:"Number",
+        required:true
+    },
+    store1order:{
+        type:"Number",
+        required:true
+    },
+    store2order:{
+        type:"Number",
+        required:true
+    },
+    store3order:{
+        type:"Number",
+        required:true
+    },
+    buyerOrder:{
+        type:"Number"
+    }
+});
+const orderModel = mongoose.model("orders",orderSchema);
+
+//To display the results on the main page
+app.get('/',(req,res) => {
+    
+    orderModel.find({},(error,result) => {
+        if(error) {
+            console.log("Display Error:"+error);
+        } 
         res.send(result);
     });
 });
 //To insert order into the database
-app.post('/add', (req, res) => {
+app.post('/add',(req,res) => {
+    
+    const date=req.body.date ,skuid=req.body.skuid,orderid=req.body.orderid;
+    const productName=req.body.productName,origin=req.body.origin,price = req.body.price;
+    const store1 = req.body.store1order,store2 = req.body.store2order,store3 = req.body.store3order;
 
-    const date = req.body.date;
-    const skuid = req.body.skuid;
-    const orderid = req.body.orderid;
-    const productName = req.body.productName;
-    const origin = req.body.origin;
-    const price = req.body.price;
-    const store1 = req.body.store1order;
-    const store2 = req.body.store2order;
-    const store3 = req.body.store3order;
-
-    const order = new orderModel({ date: date, skuid: skuid, orderid: orderid, productName: productName, origin: origin, price: price, store1order: store1, store2order: store2, store3order: store3 });
-
+    const order = new orderModel({date:date,
+        skuid:skuid,
+        orderid:orderid,
+        productName:productName,
+        origin:origin,
+        price:price,
+        store1order:store1,
+        store2order:store2,
+        store3order:store3
+    });
+    
     try {
         order.save();
         res.send("Order Placed");
-        console.log("Order Placed " + order);
+        console.log("Order Placed "+ order);
     } catch (error) {
         console.log("Insertion Error:" + error);
     }
+});
+//Updating  buyerorder together
+app.post("/update/:arrayIds&:arrayOds", async(req, res) => {
 
+    var data1 = req.params.arrayIds;
+    var data2 = req.params.arrayOds;
+    let arr1 = data1.split(",");
+    let arr2 = data2.split(",");
+
+    console.log(arr1);
+    console.log(arr2);
+
+    for (let i = 0; i < arr1.length; i++) {
+        orderModel.findByIdAndUpdate(arr1[i], { buyerOrder: arr2[i] },
+            function(err, docs) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log("Updated User : ", docs);
+                }
+            });
+    }
 });
 
-//delete list items from databse
-app.delete("/delete/:arrayIds", async(req, res) => {
+//Deleting a value
+app.delete("/delete/:arrayIds",async(req,res) => {
 
     let array = req.params.arrayIds.split(",");
 
@@ -107,23 +195,16 @@ app.delete("/delete/:arrayIds", async(req, res) => {
         orderModel.findByIdAndRemove(element).exec();
         console.log("Record Deleted");
     });
-
 });
+//Sending the array whose value is to be edited
+app.get('/:id',(req,res) =>{
+    
+    const id = req.params.id.split(",");
 
-
-//Updating an order individually
-app.put("/update", (req, res) => {
-    const id = req.body.id,
-        buyerOrder = req.body.buyerOrder;
-
-    try {
-        orderModel.findById(id, (err, order) => {
-            order.buyerOrder = buyerOrder;
-            order.save();
-            res.send("Record Updated" + order);
-            console.log("Record Updated:" + order);
-        });
-    } catch (error) {
-        console.log("Error updating:" + error);
-    }
+    orderModel.findById(id,(err,result) => {
+        if(err){
+            console.log("Can't Find:"+err);
+        }
+        res.send(result);
+    })
 });
