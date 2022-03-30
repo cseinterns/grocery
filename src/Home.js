0a2cacd6e7a1d408ff523fa97cc08a5a2dcd4f49
Shreyash163 <84655057+Shreyash163@users.noreply.github.com>
2022-03-30 18:54:43 +0530

    updated save draft function

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-import { Modal } from 'bootstrap';
 import { useEffect, useState } from 'react';
 import './App.css';
 import './bootstrap.min.css';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 
 const Home = () => {
 
@@ -10,7 +11,74 @@ const Home = () => {
     const [searchProduct,setProduct] = useState('');
     const [searchId,setID] = useState('');
 
+    const generatePdf =() =>
+    {
 
+        var columns = [
+            { title: "Date", dataKey: "Date" },
+            { title: "SKUID", dataKey: "SKUID" },
+            { title: "Order_ID", dataKey: "Order_ID" },
+        { title: "Product_Name", dataKey: "Product_Name" },
+            { title: "Origin", dataKey: "Origin" },
+            ];
+
+            var arrayg  = [];
+            orderList.map((d)=>{
+                if(d.select){
+                    arrayg.push(d);
+                }
+            })
+
+            var rows = arrayg.map((d)=>{
+                return { Date: d.date, SKUID: d.skuid, Order_ID : d.orderid , Product_Name:d.productName, Origin: d.origin  };
+            
+            })
+        
+            
+            var columns1 = [
+            
+            { title: "Store_1_Order", dataKey: "Store_1_Order" },
+            { title: "Price_$", dataKey: "Price_$" },
+            { title: "Store_2_Order", dataKey: "Store_2_Order" },
+            { title: "Store_3_Order", dataKey: "Store_3_Order" },
+            { title: "Buyer_Order", dataKey: "Buyer_Order" }
+            ]
+            
+            var rows1 =  arrayg.map((d)=>{
+               
+                return { Store_1_Order:d.store1order ,Price_$:d.price , Store_2_Order:d.store2order , Store_3_Order:d.store3order , Buyer_Order:d.buyerOrder };
+
+                })
+            
+            var today = new Date();
+                var date = today.getFullYear()+"."+today.getMonth()+"."+today.getDate();
+            var doc = new jsPDF('p', 'pt');
+            doc.setFontSize(20);
+            doc.setTextColor(40);
+            doc.text("Order report", 10, 20);
+            doc.text(`date: ${date}`, 10, 50);
+            
+            doc.autoTable(columns, rows, {
+                startY: doc.autoTableEndPosY() + 70,
+                margin: { horizontal: 10 },
+                styles: { overflow: 'linebreak' },
+                bodyStyles: { valign: 'center', halign: 'middle' },
+                columnStyles: { email: { columnWidth: 'wrap' } },
+                theme: "striped"
+            });
+            
+            doc.autoTable(columns1, rows1, {
+                startY: doc.autoTableEndPosY() + 70,
+                margin: { horizontal: 10 },
+                styles: { overflow: 'linebreak' },
+                bodyStyles: { valign: 'middle', halign: 'middle' },
+                columnStyles: { email: { columnWidth: 'wrap' } },
+                theme: "striped"
+            }); 
+            doc.save('report.pdf');
+
+
+    }
     useEffect(() =>{
         axios.get(/*("https://browser-buyer.herokuapp.com")||*/"http://localhost:8000").then((response)=>{
             setOrderList(response.data);
@@ -165,7 +233,7 @@ const Home = () => {
             </div>
 
             <button className='btn btn-primary' type='submit' onClick={updateOrder}>Submit</button>
-            <button className='btn btn-primary' type='submit'>Save Draft</button>
+            <button className='btn btn-primary' type='submit' onClick={generatePdf}>Save Draft</button>
             <a href='/add' className='btn btn-primary'>Add Order</a>
             <button type='submit' className='btn btn-primary' onClick={deleteById}>Delete Order</button>
             <button className='btn btn-primary' type='submit'>Logout</button>
